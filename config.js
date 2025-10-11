740fd5aa4036f268aa319ac7f0ff11d768baa3d0
siamhosen123 <siamprogemar0@gmail.com>
2025-10-11 19:42:58 +0600

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
             lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "lava-v4.ajieblogs.eu.org", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 443,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "https://dsc.gg/ajidevserver", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "140.238.179.182", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 2333,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "kirito", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
